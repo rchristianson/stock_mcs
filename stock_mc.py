ef27917b70ebac3f67e58ef6541503a87f069a8e
rchristianson13 <rchristianson@vt.edu>
2024-07-10 23:22:43 -0500

    added strats and mc version 1

--- stock_mc.py
+++ stock_mc.py
@@ -3,8 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 import yfinance as yf
+import importlib
+import random
+import warnings
 
-import stock_strats as strats
+strats = importlib.import_module('stock_strats')
+strats_names = [name for name in dir(strats) if callable(getattr(strats, name))]
+strats_short_names = [name[:-6] for name in strats_names]
 
 # gets the closing prices for all stocks from start_date to end_date
 def get_prices(stocks, start_date, end_date):
@@ -13,37 +18,82 @@ def get_prices(stocks, start_date, end_date):
 
 	return prices_close
 
-def buy_stock(hist_stock_df, new_day, strat):
+def buy_stock(hist_stock_df, new_day, strat, portfolio_start):
 	decision = strat(hist_stock_df)
-	profit = 0
+	portfolio_value = 0
 	for stock, weight in decision.items():
-		profit += weight * (hist_stock_df[stock].iloc[-1] - new_day[stock])
-	return(pd.Series(profit, index = new_day.index))
+		portfolio_value += portfolio_start * weight * (new_day[stock] / hist_stock_df[stock].iloc[-1])
+	return(pd.Series(portfolio_value, index = new_day.index))
 
-def single_mc(stock_prices, mc_reps, strat):
-	stock_mcs = pd.DataFrame(index = stock_prices.index[1:], columns = range(mc_reps))
+def single_mc(stock_prices, strat, portfolio_start):
+	portfolio_values = pd.Series(portfolio_start, index = [stock_prices.index[0]])
+	for i in range(1, stock_prices.shape[0]):
+		temp_value = buy_stock(stock_prices.iloc[0:i, :], stock_prices.iloc[i:(i+1), :], strat, portfolio_values.iloc[-1])
+		portfolio_values = pd.concat([portfolio_values, temp_value])
+	
+	return(portfolio_values)
+
+def mult_mc(stock_prices, mc_reps, strat, portfolio_start, sim_len):
+	if stock_prices.shape[0] <= sim_len:
+		raise valueError("Need to have more dates than simulation length.")
+	if stock_prices.shape[0] - sim_len < mc_reps:
+		warnings.warn("Less unique starting days than mc_reps. Some dates will be repeated.")
+	
+	stock_mcs = pd.DataFrame(index = range(0, sim_len), columns = range(mc_reps))
 
 	for i in range(0, mc_reps):
-		profits = [buy_stock(stock_prices[0:j], stock_prices[j:(j+1)], strat) for j in range(1, stock_prices.shape[0])]
-		stock_mcs[i] = pd.concat(profits)
-	
+		sim_start = random.randint(0, stock_prices.shape[0] - sim_len)
+		sim = single_mc(stock_prices.iloc[sim_start:(sim_start + sim_len), :], strat, portfolio_start)
+		stock_mcs.iloc[:, i] = sim
+
 	return(stock_mcs)
 
 # set parameters about the stocks and get closing prices
 end_date = dt.datetime.now()
-start_date = end_date - dt.timedelta(days=100)
-stocks = ['SPY', 'AAPL', 'MSFT']
+
+# 252 trading days in a year
+start_date = end_date - dt.timedelta(days = 252 * 2)
+
+# random assortment of tech stocks
+stocks = [
+    'AAPL',   # Apple Inc.
+    'GOOGL',  # Alphabet Inc. (Class A)
+    'GOOG',   # Alphabet Inc. (Class C)
+    'AMZN',   # Amazon.com Inc.
+    'MSFT',   # Microsoft Corporation
+    'META',   # Meta Platforms Inc.
+    'TSLA',   # Tesla, Inc.
+    'NVDA',   # NVIDIA Corporation
+    'INTC',   # Intel Corporation
+    'NFLX',   # Netflix, Inc.
+    'ADBE',   # Adobe Inc.
+    'CRM',    # Salesforce.com, Inc.
+    'PYPL',   # PayPal Holdings, Inc.
+    'CSCO',   # Cisco Systems, Inc.
+    'IBM',    # IBM (International Business Machines Corporation)
+    'SNAP',   # Snap Inc.
+    'SQ',     # Square, Inc.
+    'ZM',     # Zoom Video Communications, Inc.
+    'UBER',   # Uber Technologies, Inc.
+    'PINS'    # Pinterest, Inc.
+]
 stock_prices = get_prices(stocks, start_date, end_date)
 
 mc_reps = 100
+portfolio_start = 100000
 
-stock_mcs_rand = single_mc(stock_prices, mc_reps, strats.rand_strat)
-stock_mcs_equal = single_mc(stock_prices, mc_reps, strats.equal_strat)
+profits = pd.DataFrame()
+for strat_name in strats_short_names:
+	test_strat = getattr(strats, f'{strat_name}_strat')
+	strat_profit = mult_mc(stock_prices, mc_reps, test_strat, portfolio_start, 30)
+	start_profit = profit.mean(axis = 1)
+	profits = pd.concat([profits, strat_profit], axis = 1)
 
-stock_means_rand = stock_mcs_rand.mean(axis = 1)
-stock_means_equal = stock_mcs_equal.mean(axis = 1)
+profits.columns = strats_short_names
 
-stock_means = pd.concat([stock_means_rand, stock_means_equal], axis = 1)
-
-plt.plot(stock_means)
+plt.plot(profits)
+plt.xlabel('Days')
+plt.ylabel('Portfolio Value')
+plt.title('Portfolio Value Over Time')
+plt.legend(profits.columns)
 plt.show()
--- stock_strats.py
+++ stock_strats.py
@@ -2,13 +2,92 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import random
+import sklearn.linear_model as lm
 
 # randomly select 1 stock to buy
-def rand_strat(stock_df):
+def random_single_strat(stock_df):
 	rand_ind = random.randint(0, stock_df.shape[1] - 1)
 	return({stock_df.columns[rand_ind]: 1})
 
 # split equally between all
-def equal_strat(stock_df):
+# on average, prices go up
+def equal_distribution_strat(stock_df):
 	equal_weight = 1 / len(stock_df.columns)
 	return({stock: equal_weight for stock in stock_df.columns})
+
+# buy the stock with the biggest gain yesterday
+# winners keep winning
+def best_yesterday_strat(stock_df):
+	if stock_df.shape[0] == 1:
+		# randomly select a stock if you only have 1 row
+		rand_ind = random.randint(0, stock_df.shape[1] - 1)
+		return({stock_df.columns[rand_ind]: 1})
+	
+	row_diff = stock_df.iloc[-2] - stock_df.iloc[-1]
+	best_stock = row_diff.idxmax()
+	return({best_stock: 1})
+
+# buy the stock with the biggest loss yesterday
+# losers bounce back
+def worst_yesterday_strat(stock_df):
+	if stock_df.shape[0] == 1:
+		# randomly select a stock if you only have 1 row
+		rand_ind = random.randint(0, stock_df.shape[1] - 1)
+		return({stock_df.columns[rand_ind]: 1})
+	
+	row_diff = stock_df.iloc[-1] - stock_df.iloc[-2]
+	best_stock = row_diff.idxmax()
+	return({best_stock: 1})
+
+# buy the stock with the highest volatility
+def highest_volatility_strat(stock_df):
+	if stock_df.shape[0] < 3:
+		# randomly select a stock if you only have 1 row
+		rand_ind = random.randint(0, stock_df.shape[1] - 1)
+		return({stock_df.columns[rand_ind]: 1})
+	
+	stock_df_last = stock_df.iloc[:-1, :].values
+	stock_df_first = stock_df.iloc[1:, :].values
+	stock_returns = pd.DataFrame((stock_df_last - stock_df_first) / stock_df_first)
+	
+	volatility = stock_returns.std() * np.sqrt(252)
+	volatility.index = stock_df.columns
+	best_stock = volatility.idxmax()
+	return({best_stock: 1})
+
+# buy the stock with the lowest volatility
+def lowest_volatility_strat(stock_df):
+	if stock_df.shape[0] < 3:
+		# randomly select a stock if you only have 1 row
+		rand_ind = random.randint(0, stock_df.shape[1] - 1)
+		return({stock_df.columns[rand_ind]: 1})
+	
+	stock_df_last = stock_df.iloc[:-1, :].values
+	stock_df_first = stock_df.iloc[1:, :].values
+	stock_returns = pd.DataFrame((stock_df_last - stock_df_first) / stock_df_first)
+	
+	volatility = stock_returns.std() * np.sqrt(252)
+	volatility.index = stock_df.columns
+	best_stock = volatility.idxmin()
+	return({best_stock: 1})
+
+# buy stock trending up
+def lin_reg_strat(stock_df):
+	if stock_df.shape[0] < 2:
+		# randomly select a stock if you only have 1 row
+		rand_ind = random.randint(0, stock_df.shape[1] - 1)
+		return({stock_df.columns[rand_ind]: 1})
+	
+	X = np.arange(1, stock_df.shape[0] + 1).reshape(-1, 1)
+	reg_results = pd.Series()
+	for i in range(stock_df.shape[1]):
+		Y = stock_df.iloc[:, i].values.reshape(-1, 1)
+
+		model = lm.LinearRegression()
+		model.fit(X, Y)
+
+		reg_results[stock_df.columns[i]] = model.coef_[0][0]
+	best_stock = reg_results.idxmax()
+	return({best_stock: 1})
+
+
